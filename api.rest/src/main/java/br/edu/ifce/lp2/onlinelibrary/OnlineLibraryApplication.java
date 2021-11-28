dc43b769420de1216462d8fdc84ec1b2e2af2768
Planner Ti <dpwc.jira@plannerti.com.br>
2021-11-28 19:05:50 -0300

    classes (user e book)

---
+++ api.rest/src/main/java/br/edu/ifce/lp2/onlinelibrary/OnlineLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.lp2.onlinelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLibraryApplication.class, args);
+    }
+
+}
