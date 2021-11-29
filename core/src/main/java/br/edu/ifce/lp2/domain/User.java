d975b481dd3adcd0184b6a91fa8781270ab58e0a
Planner Ti <dpwc.jira@plannerti.com.br>
2021-11-29 17:02:16 -0300

    lista de livros favoritos

--- core/src/main/java/br/edu/ifce/lp2/domain/User.java
+++ core/src/main/java/br/edu/ifce/lp2/domain/User.java
@@ -3,10 +3,13 @@ package br.edu.ifce.lp2.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class User {
     private String id;
     private String email;
     private String password;
+    private List<Book> favoriteBooks;
 }
